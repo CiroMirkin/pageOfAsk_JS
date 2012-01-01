87750de61e4bee99177c9429499e2052849ed8d1
C.mirkin <ciromirkin@gmail.com>
2012-01-01 00:42:21 -0300

    agregado: se puede hacer un pedido y visualizarlo

--- src/modules/gestor.js
+++ src/modules/gestor.js
@@ -35,8 +35,11 @@ function getCategorieContent(categorieID) {
     return categorie.content
 }
 
+const askFromCustomer = []
+
 const categoriesContainer = document.getElementById('categories')
 const categorieListContainer = document.getElementById('categorieList')
+const showAskFromCustomerBtn = document.getElementById('showAskFromCustomerBtn')
 
 const showCategorieContent = (categorieContent) => {
     categorieListContainer.innerHTML = categorieContent.map(categorieItem => 
@@ -63,4 +66,23 @@ categoriesContainer.addEventListener('click', (e) => {
         const categorieContent = getCategorieContent(categorieName)
         showCategorieContent(categorieContent)
     }
+})
+
+categorieListContainer.addEventListener('click', e => {
+    if(e.target.classList[0] == 'item-send') {
+        const item = {
+            name: `${e.target.parentElement.innerText}`.split('  ').at(0),
+            amountOfAsk: e.target.previousElementSibling.value || 1
+        }
+        askFromCustomer.push(item)
+        e.target.previousElementSibling.value = ''
+    }
+})
+
+showAskFromCustomerBtn.addEventListener('click', () => {
+    const askFromCustomerHTMLContainer = document.getElementById('askFromCustomer')
+
+    askFromCustomerHTMLContainer.innerHTML = askFromCustomer.map(item => 
+        `<li class="list__item"> ${item.amountOfAsk} - ${item.name}</li>`
+    ).join('')
 })
